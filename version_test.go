1b469b5cad0109205135962ff82e2facca9b1846
Miguel Moll <miguel.moll@gmail.com>
2017-03-30 10:23:18 -0400

    use same resolve logic regardless of version source

--- version_test.go
+++ version_test.go
@@ -114,8 +114,8 @@ func TestNext(t *testing.T) {
 	}
 
 	next, err := vs.Next("10")
-	assert.Equal(t, "", next)
-	assert.EqualError(t, err, `current version "10" not found`)
+	assert.Equal(t, "8", next, "version 10 not found, use latest version is 8")
+	assert.Nil(t, err)
 
 	next, err = vs.Next("1")
 	assert.Equal(t, "4", next, "from version 1, next required version is 4")
@@ -217,84 +217,4 @@ func TestUnmarshalError(t *testing.T) {
 
 	_, err := All()
 	assert.EqualError(t, err, "invalid character 'o' in literal null (expecting 'u')")
-}
-
-func TestChannels(t *testing.T) {
-	edge := Versions{
-		Version{
-			Version:   "0321",
-			Published: false,
-		},
-		Version{
-			Version:   "0324",
-			Published: true,
-		},
-		Version{
-			Version:   "0325",
-			Published: false,
-		},
-		Version{
-			Version:   "0326",
-			Published: true,
-		},
-		Version{
-			Version:   "0327",
-			Published: true,
-		},
-		Version{
-			Version:   "0328",
-			Published: false,
-		},
-	}
-
-	certified := Versions{
-		Version{
-			Version:   "0324",
-			Published: true,
-		},
-		Version{
-			Version:   "0327",
-			Published: true,
-		},
-	}
-
-	// from unknown to certified
-	next, err := certified.NextCertified(edge, "0320")
-	assert.EqualError(t, err, `current version "0320" not found`)
-	assert.Equal(t, "", next)
-
-	// from certified to certified
-	next, err = certified.NextCertified(edge, "0324")
-	assert.NoError(t, err)
-	assert.Equal(t, "0327", next)
-
-	next, err = certified.NextCertified(edge, "0327")
-	assert.EqualError(t, err, `current version "0327" is latest`)
-	assert.Equal(t, "", next)
-
-	next, err = certified.NextCertified(edge, "0329")
-	assert.EqualError(t, err, `current version "0329" not found`)
-	assert.Equal(t, "", next)
-
-	// from uncertified to certified
-	next, err = certified.NextCertified(edge, "0321")
-	assert.NoError(t, err)
-	assert.Equal(t, "0324", next)
-
-	next, err = certified.NextCertified(edge, "0324")
-	assert.NoError(t, err)
-	assert.Equal(t, "0327", next)
-
-	next, err = certified.NextCertified(edge, "0325")
-	assert.NoError(t, err)
-	assert.Equal(t, "0327", next)
-
-	next, err = certified.NextCertified(edge, "0326")
-	assert.NoError(t, err)
-	assert.Equal(t, "0327", next)
-
-	next, err = certified.NextCertified(edge, "0328")
-	assert.EqualError(t, err, `current version "0328" not found`)
-	assert.Equal(t, "", next)
-
 }
